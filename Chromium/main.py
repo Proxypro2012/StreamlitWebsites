4e814ab18f0029a61a8c7feb70e495cde9d0fcde
Proxypro2012 <kabir.tiwari@outlook.com>
2025-03-22 13:36:34 -0400

    Update main.py

--- Chromium/main.py
+++ Chromium/main.py
@@ -6,7 +6,7 @@ import time
 st.set_page_config(
     layout="wide",
     page_title="Chromium",
-    page_icon="logo.png",  # Optionally, set a page icon
+    page_icon="chromium/logo.png",  # Optionally, set a page icon
     initial_sidebar_state="expanded"
 )
 
@@ -19,7 +19,7 @@ game_urls = {
 webpages = ["Home", "Games", "Status"]
 st.sidebar.header("Navigation")
 selected_page = st.sidebar.radio("", webpages)
-st.sidebar.image("logo.png")
+st.sidebar.image("chromium/logo.png")
 
 
 
