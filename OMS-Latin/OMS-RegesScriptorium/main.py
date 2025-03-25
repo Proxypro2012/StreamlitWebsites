c2481681f19661e8dfd6d07816b919a0674cb1e6
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-03-25 13:58:49 -0400

    Update main.py

--- OMS-Latin/OMS-RegesScriptorium/main.py
+++ OMS-Latin/OMS-RegesScriptorium/main.py
@@ -8,7 +8,6 @@ selected_page = st.sidebar.radio("", nav_pages)
 
 if selected_page == nav_pages[0]:
   st.title("Servius Tullius")
-  st.stream("Hello, sir we are callign from the microsofto.")
   import time
   import numpy as np
   import pandas as pd
