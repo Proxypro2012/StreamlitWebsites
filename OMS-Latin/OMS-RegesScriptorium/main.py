9ea1417731f9e83dfe81df2f8fcceee71198013b
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-03-25 13:58:10 -0400

    Update main.py

--- OMS-Latin/OMS-RegesScriptorium/main.py
+++ OMS-Latin/OMS-RegesScriptorium/main.py
@@ -9,3 +9,32 @@ selected_page = st.sidebar.radio("", nav_pages)
 if selected_page == nav_pages[0]:
   st.title("Servius Tullius")
   st.stream("Hello, sir we are callign from the microsofto.")
+  import time
+  import numpy as np
+  import pandas as pd
+  import streamlit as st
+  
+  _LOREM_IPSUM = """
+  Lorem ipsum dolor sit amet, **consectetur adipiscing** elit, sed do eiusmod tempor
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+  nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+  """
+  
+  
+  def stream_data():
+      for word in _LOREM_IPSUM.split(" "):
+          yield word + " "
+          time.sleep(0.02)
+  
+      yield pd.DataFrame(
+          np.random.randn(5, 10),
+          columns=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+      )
+  
+      for word in _LOREM_IPSUM.split(" "):
+          yield word + " "
+          time.sleep(0.02)
+  
+  
+  if st.button("Stream data"):
+      st.write_stream(stream_data)
