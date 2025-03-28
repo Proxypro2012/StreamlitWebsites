a189473042fc4138537c35b19537d4841da902fc
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-03-28 13:37:04 -0400

    Update main.py

--- OMS-Latin/OMS-RegesScriptorium/main.py
+++ OMS-Latin/OMS-RegesScriptorium/main.py
@@ -12,7 +12,7 @@ selected_page = st.sidebar.radio("", nav_pages)
 
 if selected_page == nav_pages[0]:
   st.title("The Servius Sentinel")
-  st.write("Veni, Vidi, Vici  -  I came, I saw, I conquered (Julius Ceaser)")
+  st.markdown("*Veni, Vidi, Vici - I came, I saw, I conquered (Julius Caesar)*")
 
   
   
