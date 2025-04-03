25f5f1e36c45ca1e3a4b561b8651d63aa0326a64
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-04-03 13:36:28 -0400

    Update main.py

--- OMS-Latin/OMS-RegesScriptorium/main.py
+++ OMS-Latin/OMS-RegesScriptorium/main.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 r1col1, r1col2 = st.columns([1, 0.5])
 rgap1col1, rgap1col2 = st.columns([1, 0.5])
-r2col1, r2col2 = st.columns([1, 2])  # Create two columns, one smaller (1) and one larger (2)
+r2col1, r2col2 = st.columns([1, 2])
+r3col1, r3col2 = st.columns([1, 2])# Create two columns, one smaller (1) and one larger (2)
 
 class RetrieveData:
     def __init__(self, operation):
@@ -45,6 +46,18 @@ if selected_page == nav_pages[0]:
         #st.write("Servius Tullius est sextus rex")
         st.write("Servius Tullius natus est servus")
         st.write("Tarquinnius Priscus est quintus rex. Uxor eius est Tanaquil. Tanaqil videt ignis cirum Servius caput.")
+
+    for i in range(10):
+        st.write("")
+
+    with r3col1:
+        st.header("The Rise")
+
+    with r3col2:
+        st.header("The Rule")
+
+
+    
     
     
 
