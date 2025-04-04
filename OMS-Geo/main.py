aaf6857220b1d23f503b2e1bdf7af7c56ea19fcd
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-04-04 09:48:36 -0400

    Update main.py

--- OMS-Geo/main.py
+++ OMS-Geo/main.py
@@ -10,9 +10,11 @@ progress_bar = st.progress(0, text=progress_text)
 level = 0
 levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-progress_bar.progress(level, text=progress_text)
 
-if st.button("Level 1"):
-    level = 1
-if st.button("Level 2"):
-    level = 2
+while True:
+    progress_bar.progress(level, text=progress_text)
+    
+    if st.button("Level 1"):
+        level = 1
+    if st.button("Level 2"):
+        level = 2
