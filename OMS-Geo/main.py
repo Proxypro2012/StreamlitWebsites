953de4fdb0eb221fd6f418adc94b0e07ca105154
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-04-04 10:05:30 -0400

    Update main.py

--- OMS-Geo/main.py
+++ OMS-Geo/main.py
@@ -3,7 +3,7 @@ import time
 
 r1col1, r1col2, r1col3 = st.columns([1, 2, 1])
 r2col1, r2col2, r2col3 = st.columns([1, 2, 1])
-r3col1, r3col2, r3col3 = st.columns([1, 2, 1])
+r3col1, r3col2, r3col3 = st.columns([2, 1, 2])
 r4col1, r4col2, r4col3 = st.columns([1, 2, 1])
 r5col1, r5col2, r5col3 = st.columns([1, 2, 1])
 
