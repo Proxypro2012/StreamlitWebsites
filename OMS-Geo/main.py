bb0ef49a91fe825057610b2630f54b9b143a4d8b
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-04-04 10:00:59 -0400

    Update main.py

--- OMS-Geo/main.py
+++ OMS-Geo/main.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import time
 
+r1col1, r1col2, r1col3 = (1, 2, 1)
+r2col1, r2col2, r2col3 = (1, 2, 1)
+r3col1, r3col2, r3col3 = (1, 2, 1)
+r4col1, r4col2, r4col3 = (1, 2, 1)
+r5col1, r5col2, r5col3 = (1, 2, 1)
+
 st.title("Mahatma Gandhi Quiz")
 
 progress_text = "Operation in progress. Please wait."
@@ -9,15 +15,22 @@ progress_bar = st.progress(0, text=progress_text)
 # Create the list of levels
 levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# Button interaction for selecting levels
-if st.button("Level 1"):
-    level = 1
-    for i in range(1, level + 1):
+
+level = 1
+for i in range(1, level + 1):
         progress_bar.progress(i * 10, text=(f'Question {str(level)}/10'))
         time.sleep(0.5)  # Simulating a delay for each level
 
-if st.button("Level 2"):
-    level = 2
-    for i in range(1, level + 1):
+
+level = 2
+for i in range(1, level + 1):
         progress_bar.progress(i * 10, text=(f'Question {str(level)}/10'))
         time.sleep(0.5)  # Simulating a delay for each level
+
+st.divider()
+
+with r1col2:
+    st.header("Questions")
+    
+
+
