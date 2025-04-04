cd31703a388213fb6a19ec6a1885134a7d96a9c6
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-04-04 10:06:59 -0400

    Update main.py

--- OMS-Geo/main.py
+++ OMS-Geo/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import time
 
-r1col1, r1col2, r1col3 = st.columns([0.5, 4, 0.25])
+r1col1, r1col2, r1col3 = st.columns([1, 4, 0.25])
 r2col1, r2col2, r2col3 = st.columns([0.25, 4, 0.25])
 r3col1, r3col2, r3col3 = st.columns([0.25, 4, 0.25])
 r4col1, r4col2, r4col3 = st.columns([1, 2, 1])
