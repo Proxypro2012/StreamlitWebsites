3aa9e9c3873ac3a885ded6a8db433d278d90fdf1
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-04-05 11:42:49 -0400

    Update main.py

--- OMS-Latin/OMS-RegesScriptorium/main.py
+++ OMS-Latin/OMS-RegesScriptorium/main.py
@@ -1,9 +1,12 @@
 import streamlit as st
+from streamlit_carousel import carousel
+
 
 r1col1, r1col2 = st.columns([1, 0.5])
 rgap1col1, rgap1col2 = st.columns([1, 0.5])
 r2col1, r2col2 = st.columns([1, 2])
-r3col1, r3col2, r3col3 = st.columns([2, 2, 2])# Create two columns, one smaller (1) and one larger (2)
+r3col1, r3col2, r3col3 = st.columns([2, 2, 2])
+r4col1, r4col2, r4col3 = st.columns([2, 2, 2])# Create two columns, one smaller (1) and one larger (2)
 
 class RetrieveData:
     def __init__(self, operation):
@@ -20,6 +23,37 @@ class RetrieveData:
 nav_pages = ["Home", "Credits"]
 st.sidebar.title("Navigation")
 selected_page = st.sidebar.radio("", nav_pages)
+
+items_images = [
+    dict(
+        title="Slide 1",
+        text="A tree in the savannah",
+        img="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?w=1380&t=st=1688825493~exp=1688826093~hmac=cb486d2646b48acbd5a49a32b02bda8330ad7f8a0d53880ce2da471a45ad08a4",
+        link="https://discuss.streamlit.io/t/new-component-react-bootstrap-carousel/46819",
+    ),
+    dict(
+        title="Slide 2",
+        text="A wooden bridge in a forest in Autumn",
+        img="https://img.freepik.com/free-photo/beautiful-wooden-pathway-going-breathtaking-colorful-trees-forest_181624-5840.jpg?w=1380&t=st=1688825780~exp=1688826380~hmac=dbaa75d8743e501f20f0e820fa77f9e377ec5d558d06635bd3f1f08443bdb2c1",
+        link="https://github.com/thomasbs17/streamlit-contributions/tree/master/bootstrap_carousel",
+    ),
+    dict(
+        title="Slide 3",
+        text="A distant mountain chain preceded by a sea",
+        img="https://img.freepik.com/free-photo/aerial-beautiful-shot-seashore-with-hills-background-sunset_181624-24143.jpg?w=1380&t=st=1688825798~exp=1688826398~hmac=f623f88d5ece83600dac7e6af29a0230d06619f7305745db387481a4bb5874a0",
+        link="https://github.com/thomasbs17/streamlit-contributions/tree/master",
+    ),
+    dict(
+        title="Slide 4",
+        text="PANDAS",
+        img="pandas.webp",
+    ),
+    dict(
+        title="Slide 4",
+        text="CAT",
+        img="cat.jpg",
+    ),
+]
 
 if selected_page == nav_pages[1]:
     st.title("Credits")
@@ -72,8 +106,8 @@ if selected_page == nav_pages[0]:
     with r3col3:
         st.header("The Revolt")
         st.write("**Servius Tullius** was eventually overthrown by the love intrest of his daughter **Tullia**, **Tarquinius Superbus**. **Servius** was kicked down the stairs of the **Senate House**, where he was then **ran over by his daughter** in a chariot.")
-
-    
+    with r4col2:
+        carousel(items=items_images)
     
     
 
