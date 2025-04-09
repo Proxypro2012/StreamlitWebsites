30162adeec4de833a52e17f4f533fa03479085f0
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-04-09 13:25:26 -0400

    Update main.py

--- OMS-Latin/OMS-RegesScriptorium/main.py
+++ OMS-Latin/OMS-RegesScriptorium/main.py
@@ -26,33 +26,11 @@ selected_page = st.sidebar.radio("", nav_pages)
 
 items_images = [
     dict(
-        title="Slide 1",
-        text="A tree in the savannah",
-        img="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?w=1380&t=st=1688825493~exp=1688826093~hmac=cb486d2646b48acbd5a49a32b02bda8330ad7f8a0d53880ce2da471a45ad08a4",
-        link="https://discuss.streamlit.io/t/new-component-react-bootstrap-carousel/46819",
-    ),
-    dict(
-        title="Slide 2",
-        text="A wooden bridge in a forest in Autumn",
-        img="https://img.freepik.com/free-photo/beautiful-wooden-pathway-going-breathtaking-colorful-trees-forest_181624-5840.jpg?w=1380&t=st=1688825780~exp=1688826380~hmac=dbaa75d8743e501f20f0e820fa77f9e377ec5d558d06635bd3f1f08443bdb2c1",
-        link="https://github.com/thomasbs17/streamlit-contributions/tree/master/bootstrap_carousel",
-    ),
-    dict(
         title="Slide 3",
         text="Merch for Servius Tullius",
         img="OMS-Latin/OMS-RegesScriptorium/tshirt-merch.jpeg",
         link="https://github.com/thomasbs17/streamlit-contributions/tree/master",
-    ),
-    dict(
-        title="Slide 4",
-        text="PANDAS",
-        img="pandas.webp",
-    ),
-    dict(
-        title="Slide 4",
-        text="CAT",
-        img="cat.jpg",
-    ),
+    )
 ]
 
 if selected_page == nav_pages[1]:
