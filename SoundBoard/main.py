653ba1521bc0c3d09eda530b9f1a625261708501
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-04-16 13:25:23 -0400

    Update main.py

--- SoundBoard/main.py
+++ SoundBoard/main.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+
+
+r1col1, r1col2, r1col3 = st.columns([1, 2, 1])
+r2col1, r2col2, r2col3 = st.columns([1, 2, 1])
+
+with r1col2:
+  st.title("Meme Soundboard")
+
+with r2col2:
+  st.divider()
+  
