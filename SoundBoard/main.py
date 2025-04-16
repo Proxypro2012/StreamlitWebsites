a94082db8f5f76d304c4532496f0cc79c4bd85e1
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-04-16 13:29:17 -0400

    Update main.py

--- SoundBoard/main.py
+++ SoundBoard/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 
-r1col1, r1col2, r1col3 = st.columns([0.25, 4, 0.25])
+r1col1, r1col2, r1col3 = st.columns([2, 4, 0.25])
 r2col1, r2col2, r2col3 = st.columns([0.25, 4, 0.25])
 
 with r1col2:
